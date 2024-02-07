f4eea54297bacf826d10fe358c8c6b648c1cd0a0
GiPisano <pisanogiacomo9@gmail.com>
2024-02-07 16:49:44 +0100

    add AppTitle and AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+  export default{
+  
+  };
+</script>
+
+<template>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, molestiae. Consectetur perspiciatis totam excepturi sunt nam soluta consequatur quam aut.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum ut deserunt voluptas dolores facere, excepturi voluptatem sapiente ex deleniti exercitationem quas, illo praesentium unde corrupti minus esse velit optio modi!
+    </p>
+</template>
+
+<style>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+  export default{
+    data(){
+      return{
+        title: "La mia prima app con Vite!"
+      };
+    },
+  };
+</script>
+
+<template>
+  <h1 class="title">{{ title }}</h1>
+</template>
+
+<style>
+.title{
+    text-align: center;
+}
+</style>
